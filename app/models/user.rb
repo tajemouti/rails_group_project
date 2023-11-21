52d8c3852753379a24c126e8f88121c3dd2023e6
tajemouti <tajemouti@gmail.com>
2023-11-21 16:45:48 +0100

    Fix linters' error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   has_many :foods
   has_many :recipes, foreign_key: :user_id, dependent: :destroy
   has_many :inventories, foreign_key: :user_id, dependent: :destroy
-  
 end
