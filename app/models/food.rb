00508367be6fb2a229c2118ca58f79febd2824bf
tajemouti <tajemouti@gmail.com>
2023-11-21 11:01:02 +0100

    Fix rubocop errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-    belongs_to :user
-    has_many :inventory_foods, foreign_key :food_id, dependent: :destroy
-    has_many :recipe_foods, foreign_key :food_id, dependent: :destroy
+  belongs_to :user
+  has_many :inventory_foods, foreign_key: :food_id, dependent: :destroy
+  has_many :recipe_foods, foreign_key: :food_id, dependent: :destroy
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many : recipe_foods, foreign_key: :recipe_id, dependent: :destroy
+  has_many :recipe_foods, foreign_key: :recipe_id, dependent: :destroy
 end
