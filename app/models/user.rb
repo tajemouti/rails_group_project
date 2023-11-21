e1af7c26ba8dade44815927438acd86391bef5f5
tajemouti <tajemouti@gmail.com>
2023-11-21 10:23:21 +0100

    Setup association for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :foods, foreign_key: :user_id, dependent: :destroy
+  has_many :recipes, foreign_key: :user_id, dependent: :destroy
+  has_many :inventories, foreign_key: :user_id, dependent: :destroy
 end
