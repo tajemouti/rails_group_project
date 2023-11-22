c070581fb6b428518c0d7bb16e2d238d794d77d2
tajemouti <tajemouti@gmail.com>
2023-11-22 14:54:30 +0100

    Add food validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,7 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods, foreign_key: :food_id, dependent: :destroy
   has_many :recipe_foods, foreign_key: :food_id, dependent: :destroy
+
+  validates :food, :measurement_unit, :price, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :foods
+  has_many :foods, foreign_key: :user_id, dependent: :destroy
   has_many :recipes, foreign_key: :user_id, dependent: :destroy
   has_many :inventories, foreign_key: :user_id, dependent: :destroy
+
+  validates :name, :email, presence: true
 end
