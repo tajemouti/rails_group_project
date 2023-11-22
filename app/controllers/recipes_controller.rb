0b0d5aa577d8eec2f7169a0c02c1074b5f3545f3
ngalamac <macngala4@gmail.com>
2023-11-22 17:36:14 +0100

    Implement necessary action for Recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,42 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :destroy]
+
   def index
     @recipes = current_user.recipes
   end
 
-  def show; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def new; end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
 
-  def create; end
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to recipes_url, notice: 'Recipe Added successfully.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @recipe.destroy
+    authorize! :destroy, @recipe
+    
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: 'Recipe removed successfully.' }
+    end
+  end
 
-  def update; end
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
