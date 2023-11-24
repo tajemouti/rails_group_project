584aec12309b3cdbc3c36feaa436d1c08810286e
Newton Ombese <newtonombese1@gmail.com>
2023-11-24 08:31:36 +0300

    Solve N+1 problems in the public recipes controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.where(public: true).order(updated_at: :desc)
+    @recipes = Recipe.where(public: true).includes(:recipe_foods)
   end
 end
